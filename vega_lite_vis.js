a4a6de40f6b39529c921a6e8ad432c1fcece5554
mngu0124 <mngu0124@student.monash.edu>
2024-09-22 20:16:22 +1000

    vegalite with html

---
+++ vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "2022.vg.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+    // Access the Vega view instance (you can find documentation here: https://vega.github.io/vega/docs/api/view/)
+    var view = result.view;
+}).catch(console.error);
+
