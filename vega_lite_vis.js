4e1f6ae81fd6cd1d70be9065efdfeba7c0632ffc
mngu0124 <mngu0124@student.monash.edu>
2024-10-08 17:29:59 +1100

    homework 10

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,4 @@
-var vg_1 = "2022.vg.json";
+var vg_1 = "max_temp.vg.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
-    // Access the Vega view instance (you can find documentation here: https://vega.github.io/vega/docs/api/view/)
     var view = result.view;
 }).catch(console.error);
-
